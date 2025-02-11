a307a2f6300c2f6ce9bcb0826c2b49a778462aba
kyu <knh3136@naver.com>
2025-02-11 10:57:00 +0900

    나브바

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Navbar from "./Navbar";
+import Banner from "./Banner";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  );
 }
 
 export default App;
