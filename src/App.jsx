f8522755f191d0ce7ba13103e093f5948cde13d7
kyu <knh3136@naver.com>
2025-02-11 17:48:01 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Navbar from "./component/main/Navbar";
 
 function App() {
   return (
