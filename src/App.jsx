59ea28ac9ab845d1d2818bf2c3aec31d6b0085ae
ch9729 <karlll@naver.com>
2025-02-11 15:18:46 +0900

    충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import "./App.css";
-<<<<<<< HEAD
-=======
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
->>>>>>> origin/dev
 
 function App() {
   return <></>;
